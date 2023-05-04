4744cf6d1915b3d5b1b07edd56dd65ad4a327ed6
muhamdan <danhamx2@gmail.com>
2023-05-04 19:20:32 +0700

    Update For.java

--- ptm04/src/main/java/Loops/For.java
+++ ptm04/src/main/java/Loops/For.java
@@ -9,5 +9,20 @@ package Loops;
  * @author user
  */
 public class For {
-    
-}
+    public static void main(String[] args) {
+       int isiGelas = 0;
+        int penuh = 225;
+        
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+        
+        for(isiGelas = 0 ; isiGelas <= penuh ; isiGelas++){
+           isiGelas++;
+            System.out.println("Sedang Mengisi Gelas..");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        }while(isiGelas != penuh);
+        
+        System.out.println("Finale: Isi Gelas Sekarang :" + isiGelas + "ml");
+    } 
+    }
+
