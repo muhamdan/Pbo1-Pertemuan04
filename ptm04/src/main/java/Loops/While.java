7337402014eb7b7c8c9172dd7b532a493730d6ca
muhamdan <danhamx2@gmail.com>
2023-05-04 19:10:23 +0700

    Update While.java

--- ptm04/src/main/java/Loops/While.java
+++ ptm04/src/main/java/Loops/While.java
@@ -10,8 +10,18 @@ package Loops;
  */
 public class While {
     public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+        
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+        
         while(isiGelas != Penuh){
-        isiAir();
+           isiGelas++;
+            System.out.println("Sedang Mengisi Gelas..");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
         }
+        
+        System.out.println("Finale: Isi Gelas Sekarang :" + isiGelas + "ml");
     }
 }
