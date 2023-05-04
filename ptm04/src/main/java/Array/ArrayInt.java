08c0013d73d02e2c9e899999eb00c13cd33cd81e
muhamdan <danhamx2@gmail.com>
2023-05-04 19:24:19 +0700

    Create ArrayInt.java

---
+++ ptm04/src/main/java/Array/ArrayInt.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Array;
+
+/**
+ *
+ * @author user
+ */
+public class ArrayInt {
+    public static void main(String[] args) {
+        int nilaiArr[] = {40,55,63,17,22,68,89,97,89};
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[8]);
+    }
+ 
+}
