78f9fa3c444ad400eb7346f333b79269317d6f57
muhamdan <danhamx2@gmail.com>
2023-05-04 19:13:36 +0700

    Update Do.java

--- ptm04/src/main/java/Loops/Do.java
+++ ptm04/src/main/java/Loops/Do.java
@@ -9,5 +9,19 @@ package Loops;
  * @author user
  */
 public class Do {
-    
+    public static void main(String[] args) {
+        int isiGelas = 0;
+        int penuh = 225;
+        
+        System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        System.out.println("Isi Gelas Ketika Penuh : " + penuh + "ml");
+        
+        while(isiGelas != Penuh){
+           isiGelas++;
+            System.out.println("Sedang Mengisi Gelas..");
+            System.out.println("Isi Gelas Sekarang : " + isiGelas + "ml");
+        }
+        
+        System.out.println("Finale: Isi Gelas Sekarang :" + isiGelas + "ml");
+    }
 }
